5001f4372cea5ee017d10bd6cb9e80e9252d7820
wtlin1228 <wtlin1228@gmail.com>
2023-12-12 19:41:06 +0800

    exercism - space age

---
+++ exercism/space-age/src/lib.rs
@@ -0,0 +1,39 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(Debug)]
+pub struct Duration {
+    seconds: u64,
+}
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Self { seconds: s }
+    }
+}
+
+pub trait Planet {
+    fn years_during(d: &Duration) -> f64 {
+        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+    }
+}
+
+macro_rules! planet {
+    ($x0:ident, $x1:expr) => {
+        pub struct $x0;
+        impl Planet for $x0 {
+            fn years_during(d: &Duration) -> f64 {
+                d.seconds as f64 / ($x1 * 31557600.0)
+            }
+        }
+    };
+}
+
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
