5f0705881d87a4b285685d15f29087cf49569578
wtlin1228 <wtlin1228@gmail.com>
2024-01-09 23:58:40 +0800

    exercism - allergies

---
+++ exercism/allergies/src/lib.rs
@@ -0,0 +1,56 @@
+pub struct Allergies {
+    score: u32,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+impl Allergen {
+    fn to_u32(&self) -> u32 {
+        match self {
+            Allergen::Eggs => 1,
+            Allergen::Peanuts => 2,
+            Allergen::Shellfish => 4,
+            Allergen::Strawberries => 8,
+            Allergen::Tomatoes => 16,
+            Allergen::Chocolate => 32,
+            Allergen::Pollen => 64,
+            Allergen::Cats => 128,
+        }
+    }
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self { score }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        allergen.to_u32() & self.score == allergen.to_u32()
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        [
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ]
+        .into_iter()
+        .filter(|allergen| self.is_allergic_to(allergen))
+        .collect()
+    }
+}
