473aabe66853747a2a38e219bd2a01e6088a7554
wtlin1228 <wtlin1228@gmail.com>
2024-01-29 15:38:35 +0800

    bittorrent - deal with bytes directly

--- codecrafters/bittorrent/src/lib.rs
+++ codecrafters/bittorrent/src/lib.rs
@@ -1,7 +1,8 @@
 use serde_json;
+use std::str;
 
-pub fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    match encoded_value.bytes().next().unwrap() {
+pub fn decode_bencoded_value(encoded_value: &[u8]) -> serde_json::Value {
+    match encoded_value[0] {
         b'l' => {
             return decode_array_value(encoded_value).1;
         }
@@ -17,19 +18,27 @@ pub fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     }
 }
 
-fn decode_string(encoded_value: &str) -> (&str, String) {
+fn decode_string(encoded_value: &[u8]) -> (&[u8], String) {
     // string is encoded as <number>:<string>
     //                              |        |
     //                         colon_index   |
     //                                    end_index
-    let colon_index = encoded_value.find(':').unwrap();
-    let string_length = encoded_value[..colon_index].parse::<u64>().unwrap();
+    let mut colon_index = 0;
+    while encoded_value[colon_index] != b':' {
+        colon_index += 1;
+    }
+    let string_length = str::from_utf8(&encoded_value[..colon_index])
+        .unwrap()
+        .parse::<u64>()
+        .unwrap();
     let end_index = colon_index + 1 + string_length as usize;
-    let string = &encoded_value[colon_index + 1..end_index];
-    (&encoded_value[end_index..], string.to_string())
+    let string = str::from_utf8(&encoded_value[colon_index + 1..end_index])
+        .unwrap()
+        .to_string();
+    (&encoded_value[end_index..], string)
 }
 
-fn decode_string_value(encoded_value: &str) -> (&str, serde_json::Value) {
+fn decode_string_value(encoded_value: &[u8]) -> (&[u8], serde_json::Value) {
     // string is encoded as <number>:<string>
     //                              |        |
     //                         colon_index   |
@@ -38,26 +47,32 @@ fn decode_string_value(encoded_value: &str) -> (&str, serde_json::Value) {
     (remaining, serde_json::Value::String(s))
 }
 
-fn decode_integer_value(encoded_value: &str) -> (&str, serde_json::Value) {
+fn decode_integer_value(encoded_value: &[u8]) -> (&[u8], serde_json::Value) {
     // integer is encoded as i<number>e
     //                                |
     //                             end_index
-    let end_index = encoded_value.find('e').unwrap();
-    let integer = encoded_value[1..end_index].parse::<i64>().unwrap();
+    let mut end_index = 0;
+    while encoded_value[end_index] != b'e' {
+        end_index += 1;
+    }
+    let integer = str::from_utf8(&encoded_value[1..end_index])
+        .unwrap()
+        .parse::<i64>()
+        .unwrap();
     (
         &encoded_value[end_index + 1..],
         serde_json::Value::Number(serde_json::Number::from(integer)),
     )
 }
 
-fn decode_array_value(encoded_value: &str) -> (&str, serde_json::Value) {
+fn decode_array_value(encoded_value: &[u8]) -> (&[u8], serde_json::Value) {
     // array is encoded as l<inner_encoded_value>e
     //                                           |
     //                                        end_index
     let mut encoded_value = &encoded_value[1..];
     let mut items: Vec<serde_json::Value> = vec![];
     loop {
-        match encoded_value.bytes().next().unwrap() {
+        match encoded_value[0] {
             b'e' => return (&encoded_value[1..], serde_json::Value::Array(items)),
             b'l' => {
                 let res = decode_array_value(encoded_value);
@@ -83,7 +98,7 @@ fn decode_array_value(encoded_value: &str) -> (&str, serde_json::Value) {
     }
 }
 
-fn decode_dictionary_value(encoded_value: &str) -> (&str, serde_json::Value) {
+fn decode_dictionary_value(encoded_value: &[u8]) -> (&[u8], serde_json::Value) {
     // array is encoded as d<key1><value1>...<keyN><valueN>e
     //                                                     |
     //                                                  end_index
@@ -92,7 +107,7 @@ fn decode_dictionary_value(encoded_value: &str) -> (&str, serde_json::Value) {
     loop {
         let key: String;
         let val: serde_json::Value;
-        match encoded_value.bytes().next().unwrap() {
+        match encoded_value[0] {
             b'e' => return (&encoded_value[1..], serde_json::Value::Object(map)),
             _ => {
                 let res = decode_string(encoded_value);
@@ -100,7 +115,7 @@ fn decode_dictionary_value(encoded_value: &str) -> (&str, serde_json::Value) {
                 key = res.1;
             }
         }
-        match encoded_value.bytes().next().unwrap() {
+        match encoded_value[0] {
             b'l' => {
                 let res = decode_array_value(encoded_value);
                 encoded_value = res.0;
--- codecrafters/bittorrent/tests/decode.rs
+++ codecrafters/bittorrent/tests/decode.rs
@@ -3,43 +3,54 @@ use serde_json::json;
 
 #[test]
 fn decode_bencoded_strings() {
-    assert_eq!(decode_bencoded_value("5:apple"), json!("apple"));
+    assert_eq!(decode_bencoded_value("5:apple".as_bytes()), json!("apple"));
     assert_eq!(
-        decode_bencoded_value("55:http://bittorrent-test-tracker.codecrafters.io/announce"),
+        decode_bencoded_value(
+            "55:http://bittorrent-test-tracker.codecrafters.io/announce".as_bytes()
+        ),
         json!("http://bittorrent-test-tracker.codecrafters.io/announce")
     );
 }
 
 #[test]
 fn decode_bencoded_integers() {
-    assert_eq!(decode_bencoded_value("i2131331691e"), json!(2131331691));
-    assert_eq!(decode_bencoded_value("i4294967300e"), json!(4294967300i64));
-    assert_eq!(decode_bencoded_value("i-52e"), json!(-52));
+    assert_eq!(
+        decode_bencoded_value("i2131331691e".as_bytes()),
+        json!(2131331691)
+    );
+    assert_eq!(
+        decode_bencoded_value("i4294967300e".as_bytes()),
+        json!(4294967300i64)
+    );
+    assert_eq!(decode_bencoded_value("i-52e".as_bytes()), json!(-52));
 }
 
 #[test]
 fn decode_bencoded_lists() {
-    assert_eq!(decode_bencoded_value("le"), json!([]));
+    assert_eq!(decode_bencoded_value("le".as_bytes()), json!([]));
     assert_eq!(
-        decode_bencoded_value("l5:applei169ee"),
+        decode_bencoded_value("l5:applei169ee".as_bytes()),
         json!(["apple", 169])
     );
     assert_eq!(
-        decode_bencoded_value("lli169e5:appleee"),
+        decode_bencoded_value("lli169e5:appleee".as_bytes()),
         json!([[169, "apple"]])
     );
-    assert_eq!(decode_bencoded_value("lli4eei5ee"), json!([[4], 5]));
+    assert_eq!(
+        decode_bencoded_value("lli4eei5ee".as_bytes()),
+        json!([[4], 5])
+    );
 }
 
 #[test]
 fn decode_bencoded_dictionaries() {
     assert_eq!(
-        decode_bencoded_value("d3:foo5:apple5:helloi52ee"),
+        decode_bencoded_value("d3:foo5:apple5:helloi52ee".as_bytes()),
         json!({"foo":"apple","hello":52})
     );
     assert_eq!(
         decode_bencoded_value(
-            "d10:inner_dictd4:key16:value14:key2i42e8:list_keyl5:item15:item2i3eeee"
+            "d10:inner_dictd4:key16:value14:key2i42e8:list_keyl5:item15:item2i3eeee".as_bytes()
         ),
         json!({"inner_dict":{"key1":"value1","key2":42,"list_key":["item1","item2",3]}})
     );
