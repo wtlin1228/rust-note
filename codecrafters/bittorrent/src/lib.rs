4ff3208839c9d0cbb0e8b6e6e3d116161f3d9e59
wtlin1228 <wtlin1228@gmail.com>
2024-01-31 15:38:59 +0800

    parse torrent file

--- codecrafters/bittorrent/src/lib.rs
+++ codecrafters/bittorrent/src/lib.rs
@@ -14,6 +14,61 @@ pub fn decode_bencoded_value(encoded_value: &[u8]) -> Result<serde_json::Value> 
     Ok(json)
 }
 
+pub fn parse_torrent_file(contents: &[u8]) -> Result<TorrentFile> {
+    let decoded_value = decode(contents).context("fail to decode file contents")?.1;
+
+    let mut announce: Option<&str> = None;
+    let mut length: Option<u64> = None;
+    let mut name: Option<&str> = None;
+    let mut piece_length: Option<u64> = None;
+    let mut pieces: Option<&[u8]> = None;
+    if let Decoded::Dictionary(dict) = decoded_value {
+        if let Decoded::String(s) = dict.get("announce").context("should contain announce")? {
+            announce =
+                Some(std::str::from_utf8(s).context("announce isn't in valid UTF-8 format")?);
+        };
+        if let Decoded::Dictionary(info) = dict.get("info").context("should contain info")? {
+            if let Decoded::Integer(n) = info.get("length").context("should contain length")? {
+                length = Some(n.to_owned() as u64);
+            }
+            if let Decoded::String(s) = info.get("name").context("should contain name")? {
+                name = Some(std::str::from_utf8(s).context("name isn't in valid UTF-8 format")?);
+            }
+            if let Decoded::Integer(n) = info
+                .get("piece length")
+                .context("should contain piece length")?
+            {
+                piece_length = Some(n.to_owned() as u64);
+            }
+            if let Decoded::String(s) = info.get("pieces").context("should contain pieces")? {
+                pieces = Some(s);
+            }
+        }
+    }
+
+    Ok(TorrentFile {
+        announce: announce.context("fail to get announce from torrent file")?,
+        info: TorrentFileInfo {
+            length: length.context("fail to get info.length from torrent file")?,
+            name: name.context("fail to get info.name from torrent file")?,
+            piece_length: piece_length.context("fail to get info.piece_length")?,
+            pieces: pieces.context("fail to get info.pieces")?,
+        },
+    })
+}
+
+pub struct TorrentFile<'input> {
+    pub announce: &'input str,
+    pub info: TorrentFileInfo<'input>,
+}
+
+pub struct TorrentFileInfo<'input> {
+    pub length: u64,
+    pub name: &'input str,
+    pub piece_length: u64,
+    pub pieces: &'input [u8],
+}
+
 #[derive(Debug)]
 pub enum Decoded<'input> {
     String(&'input [u8]),
--- codecrafters/bittorrent/src/main.rs
+++ codecrafters/bittorrent/src/main.rs
@@ -1,5 +1,5 @@
-use bittorrent_starter_rust::decode_bencoded_value;
-use std::env;
+use bittorrent_starter_rust::{decode_bencoded_value, parse_torrent_file};
+use std::{env, fs};
 
 // Available if you need it!
 // use serde_bencode
@@ -12,7 +12,13 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value.as_bytes()).unwrap();
-        println!("{:?}", decoded_value);
+        println!("{}", decoded_value.to_string());
+    } else if command == "info" {
+        let file_path = &args[2];
+        let contents = fs::read(file_path).unwrap();
+        let torrent_file = parse_torrent_file(&contents[..]).unwrap();
+        println!("Tracker URL: {}", torrent_file.announce);
+        println!("Length: {}", torrent_file.info.length);
     } else {
         println!("unknown command: {}", args[1])
     }
